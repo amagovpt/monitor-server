8cdb24f1e21d0295de8078c60f46166cf0c20678
João Afonso Vicente <afonso.vicente19@gmail.com>
2018-05-04 16:27:53 +0100

    some files added

---
+++ models/user.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/**
+ * User Model
+ */
+
+/**
+ * Libraries and modules
+ */
+const _ = require('lodash');
+
+/**
+ * Authtentication functions
+ */
+
+module.exports.verify = function() {
+
+}
+
+module.exports.login = function() {
+
+}
+
+/**
+ * Create functions
+ */
+
+module.exports.create = function() {
+
+}
+
+/**
+ * Get functions
+ */
+
+/**
+ * Update functions
+ */
+
+/**
+ * Delete functions
+ */
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+"use strict";
 
-/* GET users listing. */
+/**
+ * Users Router and Controller
+ */
+
+const express = require('express');
+const router = express.Router();
+const Response = require('../lib/_response');
+const User = require('../models/user');
+
+/**
+ * Root user router
+ * Does nothing - service not available
+ * @param  {[type]} req   [description]
+ * @param  {[type]} res   [description]
+ * @param  {[type]} next) {             res.send(Reponse.no_service());} [description]
+ * @return {[type]}       [description]
+ */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(Reponse.no_service());
+});
+
+
+/**
+ * [description]
+ * @param  {[type]} req   [description]
+ * @param  {[type]} res   [description]
+ * @param  {[type]} next) {             const user [description]
+ * @return {[type]}       [description]
+ */
+router.post('/login', function(req, res, next) {
+  const user = await User.login();
+  res.send(Response.success(user));
 });
 
 module.exports = router;
