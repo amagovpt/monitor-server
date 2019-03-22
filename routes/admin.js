9a3d0a9db2f746cae6f14098c61b765a87095216
Bruno Andrade <bandrade@lasige.di.fc.ul.pt>
2019-03-22 13:16:41 +0000

    post crawler

--- routes/admin.js
+++ routes/admin.js
@@ -1517,33 +1517,7 @@ router.post('/evaluations/delete', async function (req, res, next) {
     }
 });
 
-
 router.post('/page/crawler', async function (req, res, next) {
-    try {
-        req.check('domain', 'Invalid domain parameter').exists();
-        req.check('domain_id', 'Invalid domain id parameter').exists();
-        req.check('max_depth', 'Invalid depth number').exists();
-        req.check('max_pages', 'Invalid max page number').exists();
-
-        const errors = req.validationErrors();
-        console.log(req.body);
-        console.log(errors);
-        if (errors) {
-            res.send(error(new ParamsError(errors)));
-        } else {
-            let result = await get_urls(req.body.domain, req.body.max_depth, req.body.max_pages);
-            console.log(result);
-            res.send(success);
-            await create_pages(req.body.domain_id, result, []);
-
-        }
-    } catch (err) {
-        console.log(err)
-        res.send(error(new ServerError(err)));
-    }
-});
-
-router.post('/page/select_page', async function (req, res, next) {
     try {
         req.check('domain', 'Invalid domain parameter').exists();
         req.check('max_depth', 'Invalid depth number').exists();
@@ -1561,7 +1535,7 @@ router.post('/page/select_page', async function (req, res, next) {
 
         }
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.send(error(new ServerError(err)));
     }
 });
