89494246c4a2f8acfdc0a4bc07c22e8f92c467e8
João Vicente <jvicente@lasige.di.fc.ul.pt>
2019-06-26 16:27:21 +0100

    examinator by default

--- evaluators/qualweb/index.js
+++ evaluators/qualweb/index.js
@@ -5,6 +5,8 @@ const Qualweb = require('../../../../qualweb/qualweb-server/src/core');
 module.exports.init = params => {
   return Qualweb.run({
     '--act': '',
+    '--html': '',
+    '-htech': 'h37,h45,h24,h35,h36',
     '-url': params[0],
     '--save': 'json,earl'
   }, true);
