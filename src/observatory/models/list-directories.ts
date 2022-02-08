a9899d26deb9ac474ec59f94fd19f67fddd7ffc8
Joao Vicente <joao.vicente@campus.ul.pt>
2022-02-08 15:35:07 +0000

    bugs fixed

--- src/observatory/models/list-directories.ts
+++ src/observatory/models/list-directories.ts
@@ -44,7 +44,106 @@ export class ListDirectories {
       }).length;
 
     let score = 0;
-    const size = directories.length;
+
+    const tempUniquePageId = new Array<number>();
+    const tempErrorUniqueWebsiteId = new Array<number>();
+    const tempSuccessUniqueWebsiteId = new Array<number>();
+    const tempErrorUniqueTagId = new Array<number>();
+    const tempSuccessUniqueTagId = new Array<number>();
+
+    for (const row of result ?? []) {
+      if (!tempUniquePageId.includes(row.PageId)) {
+        tempUniquePageId.push(row.PageId);
+
+        const score = row.Score;
+        const floor = Math.floor(score);
+        this.frequencies[floor >= 2 ? (floor === 10 ? floor - 2 : floor - 1) : 0]++;
+
+        const errors = JSON.parse(Buffer.from(row.Errors, "base64").toString());
+        const tot = JSON.parse(Buffer.from(row.Tot, "base64").toString());
+
+        const pageErrors = errors;
+
+        for (const key in tot.results || {}) {
+          const test = tests[key]["test"];
+          const elem = tests[key]["elem"];
+          const occurrences =
+            pageErrors[test] === undefined || pageErrors[test] < 1
+              ? 1
+              : pageErrors[test];
+          const result = tests[key]["result"];
+    
+          if (result === "failed") {
+            if (!tempErrorUniqueWebsiteId.includes(row.WebsiteId)) {
+              tempErrorUniqueWebsiteId.push(row.WebsiteId);
+
+              if (this.errors[key]["n_websites"] === undefined) {
+                this.errors[key]["n_websites"] = 0;
+              }
+
+              this.errors[key]["n_websites"]++;
+            }
+
+            if (!tempErrorUniqueTagId.includes(row.TagId)) {
+              tempErrorUniqueTagId.push(row.TagId);
+
+              if (this.errors[key]["n_tags"] === undefined) {
+                this.errors[key]["n_tags"] = 0;
+              }
+
+              this.errors[key]["n_tags"]++;
+            }
+
+            if (Object.keys(this.errors).includes(key)) {
+              this.errors[key]["n_occurrences"] += occurrences;
+              this.errors[key]["n_pages"]++;
+            } else {
+              this.errors[key] = {
+                n_pages: 1,
+                n_occurrences: occurrences,
+                elem,
+                test,
+                result,
+              };
+            }
+          } else if (result === "passed") {
+            if (!tempSuccessUniqueWebsiteId.includes(row.WebsiteId)) {
+              tempSuccessUniqueWebsiteId.push(row.WebsiteId);
+
+              if (this.success[key]["n_websites"] === undefined) {
+                this.success[key]["n_websites"] = 0;
+              }
+
+              this.success[key]["n_websites"]++;
+            }
+
+            if (!tempSuccessUniqueTagId.includes(row.TagId)) {
+              tempSuccessUniqueTagId.push(row.TagId);
+
+              if (this.success[key]["n_tags"] === undefined) {
+                this.success[key]["n_tags"] = 0;
+              }
+
+              this.success[key]["n_tags"]++;
+            }
+
+
+            if (Object.keys(this.success).includes(key)) {
+              this.success[key]["n_occurrences"] += occurrences;
+              this.success[key]["n_pages"]++;
+            } else {
+              this.success[key] = {
+                n_pages: 1,
+                n_occurrences: occurrences,
+                elem,
+                test,
+                result,
+              };
+            }
+          }
+        }
+      }
+    }
 
     for (const directory of directories || []) {
       score += directory.getScore();
@@ -52,13 +151,13 @@ export class ListDirectories {
       this.A += directory.A;
       this.AA += directory.AA;
       this.AAA += directory.AAA;
-      this.frequencies = this.frequencies.map((v: number, j: number) => {
+      /*this.frequencies = this.frequencies.map((v: number, j: number) => {
         return v + directory.frequencies[j];
-      });
+      });*/
 
-      const directoryErrors = directory.errors;
+      //const directoryErrors = directory.errors;
 
-      for (const error in directoryErrors || {}) {
+      /*for (const error in directoryErrors || {}) {
         if (Object.keys(this.errors).includes(error)) {
           this.errors[error]["n_occurrences"] +=
             directoryErrors[error]["n_occurrences"];
@@ -95,7 +194,7 @@ export class ListDirectories {
             n_tags: 1,
           };
         }
-      }
+      }*/
 
       if (!this.recentPage) {
         this.recentPage = directory.recentPage;
@@ -112,7 +211,7 @@ export class ListDirectories {
       }
     }
 
-    this.score = score / size;
+    this.score = score / directories.length;
   }
 
   getScore(): number {
