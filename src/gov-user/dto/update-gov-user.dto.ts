a9448b3612796bd34b38e15af8ba1ceaba499450
Estriga97 <fc47839@alunos.fc.ul.pt>
2022-09-14 16:25:30 +0100

    fixing update

--- src/gov-user/dto/update-gov-user.dto.ts
+++ src/gov-user/dto/update-gov-user.dto.ts
@@ -1,9 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { IsNotEmpty, IsNumber } from 'class-validator';
+import { User } from 'src/user/user.entity';
 import { CreateGovUserDto } from './create-gov-user.dto';
 
 export class UpdateGovUserDto extends PartialType(CreateGovUserDto) {
     @IsNotEmpty()
     @IsNumber()
     id: number;
+    entities:User[];
 }
