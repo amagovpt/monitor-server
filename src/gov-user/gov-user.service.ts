684a30f677b169e9b7551c50d5674b642e7ca8e4
Estriga97 <fc47839@alunos.fc.ul.pt>
2022-09-14 17:08:12 +0100

    added register date

--- src/gov-user/gov-user.service.ts
+++ src/gov-user/gov-user.service.ts
@@ -18,7 +18,7 @@ export class GovUserService {
     return this.addToDB(createGovUserDto);
   }
   addToDB(createGovUserDto: CreateGovUserDto){
-    const newUser = this.govUserRepository.create(createGovUserDto);
+    const newUser = this.govUserRepository.create({registerDate: new Date(),...createGovUserDto});
     return this.govUserRepository.save(newUser);
   }
 
