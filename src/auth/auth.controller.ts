b445bdffdc976324c3f7d1d0ef3af2dee8b1db05
Estriga97 <fc47839@alunos.fc.ul.pt>
2022-10-17 17:39:31 +0100

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -53,8 +53,8 @@ export class AuthController {
   //@UseGuards()
   @Get("login")
   async loginGov(@Res() response: Response): Promise<any> {
-    const REDIRECT_URI = "http://10.55.37.16/mm2/loginRedirect";
-    const CLIENT_ID = "5679267266509668091";
+    const REDIRECT_URI = process.env.REDIRECT_URI;
+    const CLIENT_ID = process.env.CLIENT_ID;
     response.redirect(`https://preprod.autenticacao.gov.pt/oauth/askauthorization?redirect_uri=${REDIRECT_URI}&client_id=${CLIENT_ID}&response_type=token&scope=http://interop.gov.pt/MDC/Cidadao/NIC%20http://interop.gov.pt/MDC/Cidadao/NomeCompleto`);
   }
 
