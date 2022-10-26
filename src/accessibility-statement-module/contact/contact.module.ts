ec5bd172ee20155cdf887a7143db5c23f7d4ed0f
Estriga97 <fc47839@alunos.fc.ul.pt>
2022-10-26 15:48:46 +0100

    Update contact.module.ts

--- src/accessibility-statement-module/contact/contact.module.ts
+++ src/accessibility-statement-module/contact/contact.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ContactService } from './contact.service';
 import { ContactController } from './contact.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Contact } from './entities/contact.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Contact])],
   controllers: [ContactController],
   providers: [ContactService],
   exports:[ContactService]
