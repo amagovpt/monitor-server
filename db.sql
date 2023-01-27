09c051a734214c0ffd5b9f5ff9c48675523e0726
Estriga97 <fc47839@alunos.fc.ul.pt>
2023-01-27 17:07:29 +0000

    Update db.sql

--- db.sql
+++ db.sql
@@ -246,7 +246,7 @@ DROP TABLE IF EXISTS `Dump`;
 CREATE TABLE `Dump` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fileName` varchar(255) NOT NULL,
-   `ended` tinyint(1) NOT NULL DEFAULT '0',
+  `ended` tinyint(1) NOT NULL DEFAULT '0',
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UserId_UNIQUE` (`id`)
