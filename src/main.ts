6a95addc7fd8434eddce2b94be718a68fa9819d4
Estriga97 <fc47839@alunos.fc.ul.pt>
2023-03-15 16:50:34 +0000

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import compression from "compression";
 import { PageService } from "./page/page.service";
 import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -13,6 +14,15 @@ async function bootstrap() {
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
   app.use(compression());
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+
+  const config = new DocumentBuilder()
+    .setTitle('Monitor server')
+    .setDescription('The Monitor Server API description')
+    .setVersion('1.0')
+    .addTag('a11y')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 async function deletePlicas(app){
