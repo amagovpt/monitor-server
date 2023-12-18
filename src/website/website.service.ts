77b15a92fd19031e863bc7ad7487622aaaef9804
Estriga97 <fc47839@alunos.fc.ul.pt>
2023-12-18 11:31:00 +0000

    Update website.service.ts

--- src/website/website.service.ts
+++ src/website/website.service.ts
@@ -1413,7 +1413,7 @@ export class WebsiteService {
       `,
         [websitesId]
       );
-      if (pages) {
+      if (pages !== 0) {
         await queryRunner.manager.query(
           `
         DELETE FROM  
